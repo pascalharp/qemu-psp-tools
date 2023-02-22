a31a519bb89a491d00f0251b984eefa369e7ae87
Pascal Harprecht <p.harprecht@gmail.com>
2023-02-22 22:02:53 +0100

    change to bar plot

--- results/eval.py
+++ results/eval.py
@@ -27,46 +27,63 @@ if __name__ == "__main__":
 
     all_data = [ qemu_zen, qemu_zen_plus, qemu_zen_two, pspemu_zen, pspemu_zen_plus ]
 
+    qemu_data = [ qemu_zen, qemu_zen_plus, qemu_zen_two ]
+    qemu_mean = [ qemu_zen.mean, qemu_zen_plus.mean, qemu_zen_two.mean ]
+    qemu_error = [ qemu_zen.std, qemu_zen_plus.std, qemu_zen_two.std ]
+
+    pspemu_data = [ pspemu_zen, pspemu_zen_plus ]
+    pspemu_mean = [ pspemu_zen.mean, pspemu_zen_plus.mean ]
+    pspemu_error = [ pspemu_zen.std, pspemu_zen_plus.std ]
+
     qlabels = []
     ulabels = []
-    fig, (qdata, udata) = plt.subplots(2, 1)
-    #fig.tight_layout()
+    fig, (qax, uax) = plt.subplots(2, 1)
+
+    qax.bar(np.arange(len(qemu_data)), [ x.mean for x in qemu_data ], yerr=[ x.std for x in qemu_data ], align='center', alpha=0.5, ecolor='black', color=['red', 'green', 'blue'], capsize=10)
+    qax.set_xticks(np.arange(len(qemu_data)))
+    qax.set_xticklabels([ x.name for x in qemu_data ])
+
+    ## add fake shadow plot for PSPEmu Zen2
+    uax.bar(np.arange(len(pspemu_data) + 1), [ x.mean for x in pspemu_data ] + [0.0], yerr=[ x.std for x in pspemu_data ] + [0], align='center', alpha=0.5, ecolor='black', color=['red', 'green', 'blue'], capsize=10)
+    uax.set_xticks(np.arange(len(pspemu_data) + 1))
+    uax.set_ylim(bottom=3000)
+    uax.set_xticklabels([ x.name for x in pspemu_data ] + ["PSPEmu Zen2"])
 
 
-    qdata.set_title("QEMU-PSP")
-    qdata.set_xticklabels([])
-    qdata.set_xticks([])
-    qviolins = qdata.violinplot([qemu_zen.raw, qemu_zen_plus.raw, qemu_zen_two.raw], widths=0.8)
-    qviolins['cbars'].set_color('grey')
-    qviolins['cmins'].set_color('grey')
-    qviolins['cmaxes'].set_color('grey')
+#    qdata.set_title("QEMU-PSP")
+#    qdata.set_xticklabels([])
+#    qdata.set_xticks([])
+#    qviolins = qdata.violinplot([qemu_zen.raw, qemu_zen_plus.raw, qemu_zen_two.raw], widths=0.8)
+#    qviolins['cbars'].set_color('grey')
+#    qviolins['cmins'].set_color('grey')
+#    qviolins['cmaxes'].set_color('grey')
+#
+#    qvio = qviolins['bodies']
+#    qvio[0].set_color('red')
+#    qlabels.append((mpatch.Patch(color='red'), "Zen"))
+#    qvio[1].set_color('green')
+#    qlabels.append((mpatch.Patch(color='green'), "Zen+"))
+#    qvio[2].set_color('blue')
+#    qlabels.append((mpatch.Patch(color='blue'), "Zen2"))
+#    qdata.legend(*zip(*qlabels), loc=1)
+#
+#
+#    udata.set_title("PSPEmu")
+#    udata.set_xticklabels([])
+#    udata.set_xticks([])
+#    uviolins = udata.violinplot([pspemu_zen.raw, pspemu_zen_plus.raw], widths=0.8)
+#    uviolins['cbars'].set_color('grey')
+#    uviolins['cmins'].set_color('grey')
+#    uviolins['cmaxes'].set_color('grey')
+#
+#    uvio = uviolins['bodies']
+#    uvio[0].set_color('red')
+#    ulabels.append((mpatch.Patch(color='red'), "Zen"))
+#    uvio[1].set_color('green')
+#    ulabels.append((mpatch.Patch(color='green'), "Zen+"))
+#    udata.legend(*zip(*ulabels), loc=1)
 
-    qvio = qviolins['bodies']
-    qvio[0].set_color('red')
-    qlabels.append((mpatch.Patch(color='red'), "Zen"))
-    qvio[1].set_color('green')
-    qlabels.append((mpatch.Patch(color='green'), "Zen+"))
-    qvio[2].set_color('blue')
-    qlabels.append((mpatch.Patch(color='blue'), "Zen2"))
-    qdata.legend(*zip(*qlabels), loc=1)
-
-
-    udata.set_title("PSPEmu")
-    udata.set_xticklabels([])
-    udata.set_xticks([])
-    uviolins = udata.violinplot([pspemu_zen.raw, pspemu_zen_plus.raw], widths=0.8)
-    uviolins['cbars'].set_color('grey')
-    uviolins['cmins'].set_color('grey')
-    uviolins['cmaxes'].set_color('grey')
-
-    uvio = uviolins['bodies']
-    uvio[0].set_color('red')
-    ulabels.append((mpatch.Patch(color='red'), "Zen"))
-    uvio[1].set_color('green')
-    ulabels.append((mpatch.Patch(color='green'), "Zen+"))
-    udata.legend(*zip(*ulabels), loc=1)
-
-    fig.supylabel("elapsed time in ns")
+    fig.supylabel("elapsed time in ms")
     plt.show()
 
     print(*all_data, sep='\n')
